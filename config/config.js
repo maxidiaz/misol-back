94d8903581caee74cfa8306f94b5c148cf0c676b
Maxi Diaz <m.diaz@globallogic.com>
2016-12-29 16:06:09 -0300

    cambio en variables de entorno2

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ const mongodbConnectionPORT = process.env.MONGODB_SERVICE_PORT || 27017
 const mongodbUser = 'm.diaz'
 const mongodbPassword = '100213..'
 
-const hostIP = process.env.NODEJS_SERVICE_HOST || 'localhost'
-const hostPORT = process.env.NODEJS_SERVICE_PORT || 3000
+const hostIP = process.env.MISOL_SERVICE_HOST || 'localhost'
+const hostPORT = process.env.MISOL_SERVICE_PORT || 3000
 
 const config = {
   dbConnectionUrl : `mongodb://${mongodbUser}:${mongodbPassword}@${mongodbConnectionIP}:${mongodbConnectionPORT}/misol`,
