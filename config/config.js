07702e077f1e19312fb508c9813f5c6e1e841f76
Maxi Diaz <m.diaz@globallogic.com>
2016-12-29 11:33:10 -0300

    remove static path

--- config/config.js
+++ config/config.js
@@ -1,10 +1,5 @@
 const fs = require('fs')
 
-const tls = {
-  key: fs.readFileSync('C:/Users/m.diaz/Proyectos/MiSol/keys/server.key'),
-  cert: fs.readFileSync('C:/Users/m.diaz/Proyectos/MiSol/keys/server.crt')
-}
-
 const config = {
   dbConnectionUrl : 'mongodb://127.0.0.1:27017/misol',
   serverConnection:{
