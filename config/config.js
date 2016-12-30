8451c1e53c00c57fbd0a1332c08bdc54e419b4e9
Maxi Diaz <m.diaz@globallogic.com>
2016-12-30 10:51:58 -0300

    cambio en variables de entorno10

--- config/config.js
+++ config/config.js
@@ -5,8 +5,11 @@ const mongodbConnectionPORT = process.env.MONGODB_SERVICE_PORT || 27017
 const mongodbUser = 'm.diaz'
 const mongodbPassword = '100213..'
 
-const hostIP = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '10.1.93.19'
-const hostPORT = process.env.MISOL_SERVICE_PORT || 3000
+const hostIP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '10.1.93.19'
+const hostPORT = process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+console.log('OPENSHIFT_NODEJS_IP : ' + process.env.OPENSHIFT_NODEJS_IP)
+console.log('OPENSHIFT_NODEJS_PORT : ' + process.env.OPENSHIFT_NODEJS_PORT)
 
 const config = {
   dbConnectionUrl : `mongodb://${mongodbUser}:${mongodbPassword}@${mongodbConnectionIP}:${mongodbConnectionPORT}/misol`,
