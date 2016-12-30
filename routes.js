943b2341813b53a1b9a34479ff600f6d397106b6
Maxi Diaz <m.diaz@globallogic.com>
2016-12-30 15:52:23 -0300

    arreglando rutas front2

--- routes.js
+++ routes.js
@@ -3,37 +3,40 @@ const OrderController = require('./controllers/OrderController')
 const CategoryController = require('./controllers/CategoryController')
 const UserController = require('./controllers/UserController')
 
+const apiPrefix = '/api'
+
 const routes = [
-  {method:'GET',path:'/',handler(req,res){
+  {method:'GET',path: apiPrefix + '/',handler(req,res){
         res.file('./app/index.html')
       }
   },
+
   // Varieties
-  {method:'POST',path:'/variety',config:VarietyController.save},
-  {method:'GET',path:'/variety',config:VarietyController.list},
-  {method:'GET',path:'/variety/{id}',config:VarietyController.find},
-  {method:'DELETE',path:'/variety/{id}',config:VarietyController.remove},
+  {method:'POST',path: apiPrefix + '/variety',config:VarietyController.save},
+  {method:'GET',path: apiPrefix + '/variety',config:VarietyController.list},
+  {method:'GET',path: apiPrefix + '/variety/{id}',config:VarietyController.find},
+  {method:'DELETE',path: apiPrefix + '/variety/{id}',config:VarietyController.remove},
 
   // Categories
-  {method:'POST',path:'/category',config:CategoryController.save},
-  {method:'GET',path:'/category',config:CategoryController.list},
-  {method:'GET',path:'/category/{id}',config:CategoryController.find},
-  {method:'GET',path:'/category/name/{name}',config:CategoryController.findByName},
-  {method:'PUT',path:'/category/{id}',config:CategoryController.update},
-  {method:'DELETE',path:'/category/{id}',config:CategoryController.remove},
+  {method:'POST',path: apiPrefix + '/category',config:CategoryController.save},
+  {method:'GET',path: apiPrefix + '/category',config:CategoryController.list},
+  {method:'GET',path: apiPrefix + '/category/{id}',config:CategoryController.find},
+  {method:'GET',path: apiPrefix + '/category/name/{name}',config:CategoryController.findByName},
+  {method:'PUT',path: apiPrefix + '/category/{id}',config:CategoryController.update},
+  {method:'DELETE',path: apiPrefix + '/category/{id}',config:CategoryController.remove},
 
   // Orders
-  {method:'POST',path:'/order',config:OrderController.save},
-  {method:'GET',path:'/order',config:OrderController.list},
-  {method:'GET',path:'/order/{id}',config:OrderController.find},
-  {method:'PUT',path:'/order',config:OrderController.update},
-  {method:'GET',path:'/order/date/{datetime}',config:OrderController.findByDate},
+  {method:'POST',path: apiPrefix + '/order',config:OrderController.save},
+  {method:'GET',path: apiPrefix + '/order',config:OrderController.list},
+  {method:'GET',path: apiPrefix + '/order/{id}',config:OrderController.find},
+  {method:'PUT',path: apiPrefix + '/order',config:OrderController.update},
+  {method:'GET',path: apiPrefix + '/order/date/{datetime}',config:OrderController.findByDate},
 
   // Users
-  {method:'POST',path:'/user',config:UserController.save},
-  {method:'GET',path:'/user',config:UserController.list},
-  {method:'GET',path:'/user/{username}',config:UserController.find},
-  {method:'DELETE',path:'/user/{username}',config:UserController.remove}
+  {method:'POST',path: apiPrefix + '/user',config:UserController.save},
+  {method:'GET',path: apiPrefix + '/user',config:UserController.list},
+  {method:'GET',path: apiPrefix + '/user/{username}',config:UserController.find},
+  {method:'DELETE',path: apiPrefix + '/user/{username}',config:UserController.remove}
 
 ]
 
