32584f65b007560b99e317589733983daf8676a5
Maxi Diaz <m.diaz@globallogic.com>
2016-12-31 12:57:03 -0300

    cambio en notification4

--- app/src/utils/Notifications.js
+++ app/src/utils/Notifications.js
@@ -3,9 +3,9 @@ export const newOrderNotification = (data) => {
     title: 'Nuevo pedido',
     body: 'Un pedido a nombre de ' + data.name,
     icon: '/assets/app/platform/android/res/drawable-xxxhdpi/appicon.png',
-    click_action: 'https://misol.herokuapp.com/#/orders',
+    click_action: 'https://misol.herokuapp.com/#/orders/detail/'+data._id,
     data: {
-      actionUrl: '/orders'
+      actionUrl: '/orders/detail/'+data._id
     }
   }
 }
