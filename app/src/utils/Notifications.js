c22ee7a0854d3dda1d70d9c604d529bc0dc70faf
Maxi Diaz <m.diaz@globallogic.com>
2016-12-31 12:41:58 -0300

    cambiando imagen de notification

--- app/src/utils/Notifications.js
+++ app/src/utils/Notifications.js
@@ -2,7 +2,7 @@ export const newOrderNotification = (data) => {
   return {
     title: 'Nuevo pedido',
     body: 'Un pedido a nombre de ' + data.name,
-    icon: '/assets/app/platform/android/res/drawable-hdpi/appicon.png',
+    icon: '/assets/app/platform/android/res/drawable-xxxhdpi/appicon.png',
     click_action: 'https://misol.herokuapp.com/#/orders',
     data: {
       actionUrl: '/orders'
